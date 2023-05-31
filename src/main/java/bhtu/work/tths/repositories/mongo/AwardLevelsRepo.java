f8605dea4bbd57703339f1f2114b93e58fe43ba0
tu <bhtu.work@gmail.com>
2023-05-31 17:35:20 +0700

    prev commit

--- src/main/java/bhtu/work/tths/repositories/mongo/AwardLevelsRepo.java
+++ src/main/java/bhtu/work/tths/repositories/mongo/AwardLevelsRepo.java
@@ -3,11 +3,9 @@ package bhtu.work.tths.repositories.mongo;
 import java.time.LocalDate;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import bhtu.work.tths.models.AwardLevelPeriod;
 
-@Repository
 public interface AwardLevelsRepo extends MongoRepository<AwardLevelPeriod, LocalDate> {
 
 }
