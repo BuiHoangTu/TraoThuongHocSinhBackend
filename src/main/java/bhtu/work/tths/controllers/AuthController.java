38f1a81c98e018b242914fb6020be3b137d47664
tu <bhtu.work@gmail.com>
2023-06-13 17:25:28 +0700

    support refresh page

--- src/main/java/bhtu/work/tths/controllers/AuthController.java
+++ src/main/java/bhtu/work/tths/controllers/AuthController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 
 @RestController
 @RequestMapping("api/auth")
@@ -32,6 +34,11 @@ public class AuthController {
     @PostMapping("signup")
     public ResponseEntity<?> registerParent(@Valid @RequestBody SignupRequest signUpRequest) {
         return this.authService.registerParent(signUpRequest);
+    }
+
+    @RequestMapping("user")
+    public Principal user(Principal user) {
+        return user;
     }
     // #endregion
 }
