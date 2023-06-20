f49c20a528717f847ad083792d9257cfa7e79558
tu <bhtu.work@gmail.com>
2023-06-20 15:53:54 +0700

    rename open api controller

--- src/main/java/bhtu/work/tths/controllers/OpenController.java
+++ src/main/java/bhtu/work/tths/controllers/OpenController.java
@@ -3,7 +3,6 @@ package bhtu.work.tths.controllers;
 import bhtu.work.tths.models.User;
 import bhtu.work.tths.repositories.mongo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/open")
-public class HomeController {
+public class OpenController {
 
     @Autowired
     private UserRepo userRepo;
