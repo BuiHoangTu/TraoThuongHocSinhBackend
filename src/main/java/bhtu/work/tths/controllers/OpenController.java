f9783ec2aa40f576ceaf0ed53964bc8324d24f55
tu <bhtu.work@gmail.com>
2023-07-05 16:10:41 +0700

    remove some open api

--- src/main/java/bhtu/work/tths/controllers/OpenController.java
+++ src/main/java/bhtu/work/tths/controllers/OpenController.java
@@ -1,11 +1,7 @@
 package bhtu.work.tths.controllers;
 
-import bhtu.work.tths.models.User;
-import bhtu.work.tths.repositories.mongo.UserRepo;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,27 +13,10 @@ import java.util.Collections;
 public class OpenController {
     private static final Logger logger = LoggerFactory.getLogger(OpenController.class);
 
-    private final UserRepo userRepo;
-
-    @Autowired
-    public OpenController(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
 
     @GetMapping("server-identity")
     public ResponseEntity<?> getServerIdentity() {
         logger.info("An user has access server-identity");
         return ResponseEntity.ok().body(Collections.singletonMap("string", "Welcome from TraoThuongHocSinhBackend java-spring-3"));
-    }
-
-    @GetMapping("username")
-    public ResponseEntity<User> checkUserExist(@RequestParam(name = "username", defaultValue = "admin") String username) {
-        return ResponseEntity.ok().body(this.userRepo.findByUsername(username).orElseThrow(() -> null));
-    }
-
-    @PostMapping("check-input")
-    public ResponseEntity<?> checkInput(@RequestBody String whole) {
-        logger.info("An user says hi: " + whole);
-        return ResponseEntity.ok().body(Collections.singletonMap("string", "recieved, check backend console"));
     }
 }
