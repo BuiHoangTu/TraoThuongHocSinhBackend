1bb154c0056e30b9f251fd4854c8ff34fb687d0b
tu <bhtu.work@gmail.com>
2023-07-05 16:15:39 +0700

    put MongoConfig back

---
+++ src/main/java/bhtu/work/tths/repositories/mongo/Config.java
@@ -0,0 +1,49 @@
+package bhtu.work.tths.repositories.mongo;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "bhtu.work.tths")
+public class Config extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+
+    @Override
+    protected String getDatabaseName() {
+        return this.databaseName;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        final ConnectionString connectionString = new ConnectionString(connectionUri);
+        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Override
+    public Collection<String> getMappingBasePackages() {
+        return Collections.singleton("bhtu.work.tths");
+    }
+
+    @Override
+    protected boolean autoIndexCreation() {
+        return true;
+    }
+}
